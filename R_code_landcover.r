5e30486b367db06943d678b1d51d75e730fdc574
TBergamini <62594472+TBergamini@users.noreply.github.com>
2020-04-21 16:25:54 +0200

    Create R_code_landcover.r

---
+++ R_code_landcover.r
@@ -0,0 +1,27 @@
+# R code land cover
+
+setwd("C:/lab")
+
+library(raster)
+
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+
+install.packages("RStoolbox")
+library("RStoolbox")
+
+#landstat bands: 1b, 2g, 3r, 4nir
+
+#rgb
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
+p224r63_2011c <- unsuperClass(p224r63_2011, nClasses=4)
+ 
+p224r63_2011c
+
+plot(p224r63_2011c$map)
+
+clclass <- colorRampPalette(c('red', 'green', 'blue', 'black'))(100) 
+plot(p224r63_2011c$map, col=clclass)
+
+p224r63_2011c <- unsuperClass(p224r63_2011, nClasses=2)
+plot(p224r63_2011c$map)
